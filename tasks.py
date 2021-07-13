71b159ba6e08a0a36a4028c75653be49ffc9442d
zordon-team <zordon@eventbrite.com>
2021-07-13 09:39:59 -0300

    Removed CircleCI

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke_release.tasks import *  # noqa: F403
-
-
-configure_release_parameters(  # noqa: F405
-    module_name='celery',
-    display_name='Celery',
-    use_pull_request=True,
-    use_tag=False,
-)
